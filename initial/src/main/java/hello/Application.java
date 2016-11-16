b0ce6aa336a314a3245d697ff24a6687fc3e2764
N3ldor3th <n3ldor3th@gmail.com>
2016-11-16 12:17:24 +0100

    testing rebase

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -22,6 +22,7 @@ public class Application {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 	static OracleDataSource ods;
 
+
 	public static void main(String args[]) {
 		SpringApplication.run(Application.class);
 		try {
