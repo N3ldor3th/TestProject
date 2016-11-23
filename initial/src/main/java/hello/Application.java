4ea1dbffa777653fe48f767baefc0d69963d0aea
Jan Kuznik <jan.kuznik@tieto.com>
2016-11-23 16:39:27 +0100

    Beans for liquibase and datasource moved to Configurator class

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -1,48 +1,13 @@
 package hello;
 
-import liquibase.integration.spring.SpringLiquibase;
-import oracle.jdbc.pool.OracleDataSource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
-
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
-    @Value("${spring.datasource.url}")
-    private String url;
-
-
     public static void main(String args[]) {
 		SpringApplication.run(Application.class);
 	}
-
-    @Bean
-	public DataSource connect() throws SQLException{
-		OracleDataSource dataSource = new OracleDataSource();
-        dataSource.setUser(username);
-		dataSource.setPassword(password);
-		dataSource.setURL(url);
-		dataSource.setImplicitCachingEnabled(true);
-		dataSource.setFastConnectionFailoverEnabled(true);
-		return dataSource;
-	}
-
-    @Bean
-    public SpringLiquibase liquibase() throws SQLException {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setChangeLog("classpath:changeLog.xml");
-        liquibase.setDataSource(connect());
-        return liquibase;
-    }
 }
---
+++ initial/src/main/java/hello/Configuration.java
@@ -0,0 +1,40 @@
+package hello;
+
+import liquibase.integration.spring.SpringLiquibase;
+import oracle.jdbc.pool.OracleDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by kuznijan on 23-Nov-16.
+ */
+
+@Component
+public class Configuration {
+    @Value("${spring.datasource.username}") private String username;
+    @Value("${spring.datasource.password}") private String password;
+    @Value("${spring.datasource.url}") private String url;
+
+    @Bean
+    public DataSource connect() throws SQLException {
+        OracleDataSource dataSource = new OracleDataSource();
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        dataSource.setURL(url);
+        dataSource.setImplicitCachingEnabled(true);
+        dataSource.setFastConnectionFailoverEnabled(true);
+        return dataSource;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() throws SQLException {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:changeLog.xml");
+        liquibase.setDataSource(connect());
+        return liquibase;
+    }
+}
