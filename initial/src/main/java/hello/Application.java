3d36fcb62a558b41e60625e5f88973e6c5634166
Jan Kuznik <jan.kuznik@tieto.com>
2016-11-23 16:35:43 +0100

    ScheduledConnector created and scheduled method moved there

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -2,27 +2,18 @@ package hello;
 
 import liquibase.integration.spring.SpringLiquibase;
 import oracle.jdbc.pool.OracleDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.client.RestTemplate;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
-	private final RestTemplate restTemplate = new RestTemplate();
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     @Value("${spring.datasource.username}")
     private String username;
@@ -34,27 +25,6 @@ public class Application {
 
     public static void main(String args[]) {
 		SpringApplication.run(Application.class);
-	}
-	
-	@Scheduled(fixedRate = 3000)
-	public void getQuote (){
-
-		try {
-			Quote quote = restTemplate.getForObject(
-					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-			log.info(quote.toString());
-            OracleDataSource ods = (OracleDataSource) connect();
-			Connection conn = ods.getConnection();
-			PreparedStatement pst;
-            pst = conn.prepareStatement ("insert into QUOTE (TYPE, TEXT) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
-            pst.setString(1,quote.getType());
-			pst.setString(2,quote.getValue().getQuote());
-            //pst.setInt(3,quote.getValue().getId().intValue());
-			pst.execute();
-			pst.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 	}
 
     @Bean
---
+++ initial/src/main/java/hello/ScheduledConnector.java
@@ -0,0 +1,45 @@
+package hello;
+
+import oracle.jdbc.pool.OracleDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by kuznijan on 23-Nov-16.
+ */
+@Component
+public class ScheduledConnector {
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    @Autowired private DataSource ods;
+
+    @Scheduled(fixedRate = 3000)
+    public void getQuote (){
+
+        try {
+            final Quote quote = restTemplate.getForObject(
+                    "http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+            final Connection conn = ods.getConnection();
+            final PreparedStatement pst;
+            pst = conn.prepareStatement ("insert into QUOTE (TYPE, TEXT) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+            pst.setString(1,quote.getType());
+            pst.setString(2,quote.getValue().getQuote());
+            //pst.setInt(3,quote.getValue().getId().intValue());
+            pst.execute();
+            pst.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
